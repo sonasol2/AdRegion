116ea24e096a66ac6aa23ed5a488ea55facbdfe7
sonasol2 <57984322+sonasol2@users.noreply.github.com>
2025-06-15 16:57:29 +0200

    fix search

--- AdvertisingRegionService.API/Services/PredicateSearchFactory.cs
+++ AdvertisingRegionService.API/Services/PredicateSearchFactory.cs
@@ -8,11 +8,12 @@ public class PredicateSearchFactory
     public static List<SearchPredicate> CreateSearchPredicates(SearchRequest searchRequest) // вынес сюда чтобы сделать контроллер чище
     {
         var searchPredicates = new List<SearchPredicate>();
-
-        if (!string.IsNullOrEmpty(searchRequest.SearchText)) 
+        var searchText = searchRequest.SearchText.ToLower();
+        
+        if (!string.IsNullOrEmpty(searchText)) 
             searchPredicates.Add(platform =>
                 platform != null &&
-                platform.Region.RegionName.Contains(searchRequest.SearchText.ToLower()));
+                platform.Region.RegionName.ToLower() == searchText);
         
         if(searchRequest.PostedAt != default)
             searchPredicates.Add(platform => 
